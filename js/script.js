9e605611d3fedf43ded8304d617b80a9b37c2394
Maria6112 <mariastefanian@gmail.com>
2023-09-08 15:39:20 +0300

    Movie Details Page

--- js/script.js
+++ js/script.js
@@ -95,6 +95,65 @@ async function displayPopularShows() {
     });
 }
 
+// Display Movie Details
+async function displayMovieDetails() {
+    const movieId = window.location.search.split('=')[1];
+    //window.location.search show to us the ?id=615656.
+    // we need to add .split to get only number of the id
+    // enter the index number to show only number
+    // console.log(movieId); 
+
+    const movie = await fetchAPIData(`movie/${movieId}`);
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="details-top">
+          <div>
+            ${
+            movie.poster_path
+                ? `<img 
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />` : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+            }
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre)=>`<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies.map((company)=>`<span>${company.name}</span>`).join(', ')}</div>
+        </div>
+    `;
+    document.querySelector('#movie-details').appendChild(div);
+
+}
+
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint) {
@@ -131,6 +190,9 @@ function higlightActive() {
         });
 }
 
+function addCommasToNumber (number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 
 //Init App
 function init() {
@@ -144,7 +206,7 @@ function init() {
             displayPopularShows();
             break;
         case '/movie-details.html':
-            console.log('Movie Details');
+            displayMovieDetails();
             break;
         case '/tv-details.html':
             console.log('TV Details');
